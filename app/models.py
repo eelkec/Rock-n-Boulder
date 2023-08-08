622eab5631ebb2db662417c626611d4a95776be1
eelkec <21316@burnside.school.nz>
2023-08-09 11:59:42 +1200

    Created main page
    created a page that displays posts and information - potentially a main page

--- app/models.py
+++ app/models.py
@@ -3,16 +3,19 @@ from app import db
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(50))
+    username = db.Column(db.String(50))
+    password = db.Column(db.String(50))
+    first_name = db.Column(db.String(50))
+    last_name = db.Column(db.String(50))
     email = db.Column(db.String(50))
 
     def __repr__(self):
         return '<User %r>' % self.name
     
-class Boulder(db.Model):
+class Climb(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     user_id = db.Column(db.Integer, foreign_key=True)
-    name = db.Column(db.String(120))
+    name = db.Column(db.String(50))
     image = db.Column(db.String(80))
     grade = db.Column(db.String(50))
     description = db.Column(db.String(120))
